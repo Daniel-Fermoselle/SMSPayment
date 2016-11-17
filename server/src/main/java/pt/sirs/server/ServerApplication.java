9d70cd51a1b9f372ce8abfca5b3589788a5dba42
TiaGoMSR <tiagomsr4@gmail.com>
2016-11-17 15:32:05 +0000

    Server is now creating a socket and waiting for connection Added the server implementation in class ServerApplication

--- server/src/main/java/pt/sirs/server/ServerApplication.java
+++ server/src/main/java/pt/sirs/server/ServerApplication.java
@@ -1,12 +1,49 @@
 package pt.sirs.server;
 
-
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import pt.sirs.smsPacket.smsPacket;
+import pt.sirs.server.Server;
 
 public class ServerApplication {
+	
+	public static final int SERVER_PORT = 100000;
+	//Number of connects that the server will have on his queue
+	public static final int QUEUE_SIZE = 10;
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    	ServerSocket providerSocket = null;
+    	ObjectOutputStream out = null;
+    	ObjectInputStream in = null;
+        try{
+            //1. creating a server socket
+            providerSocket = new ServerSocket(SERVER_PORT, QUEUE_SIZE);
+            //2. Wait for connection
+            System.out.println("Waiting for connection");
+            Socket connection = providerSocket.accept();
+            System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+            //3. get Input and Output streams
+            out = new ObjectOutputStream(connection.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(connection.getInputStream());
+            
+            //INSERT SERVER CODE HERE
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            //4: Closing connection
+            try{
+                in.close();
+                out.close();
+                providerSocket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
     }
 
 }
