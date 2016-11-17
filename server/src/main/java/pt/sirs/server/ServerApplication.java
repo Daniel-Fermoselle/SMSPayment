3a1961a443aaee6e870f3735cc18bce9bd70d9a3
TiaGoMSR <tiagomsr4@gmail.com>
2016-11-17 14:29:51 +0000

    Added server folders and .gitignores

---
+++ server/src/main/java/pt/sirs/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package pt.sirs.server;
+
+
+
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
