82af6ef08db2f070e044c1820309ddf99fb2385d
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2016-11-19 11:02:18 +0000

    Added signature parameter to smsPacket class in order to proceed to the cipher protection of sms

--- msgPacket/src/main/java/pt/sirs/smsPacket/smsPacket.java
+++ msgPacket/src/main/java/pt/sirs/smsPacket/smsPacket.java
@@ -1,5 +1,9 @@
 package pt.sirs.smsPacket;
 import java.io.Serializable;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.security.SignatureException;
+
 import pt.sirs.smsPacket.Exceptions.InvalidSMSPacketValuesException;
 
 
@@ -7,12 +11,14 @@ public class smsPacket implements  Serializable{
 	private static final long serialVersionUID = 1L;
 	private String iban;
 	private String amount;
-	//private cena de cifra
+	private Signature sig;
 	
 	public smsPacket(String ib, String am) throws InvalidSMSPacketValuesException{
 		if(ib.length()==25 && am.length()<=8){
 			iban=ib;
 			amount=am;
+			//sig=null;//CUIDADO
+			
 		}
 		else{
 			throw new InvalidSMSPacketValuesException(ib,am);
@@ -30,6 +36,31 @@ public class smsPacket implements  Serializable{
 	}
 	public String getAmount(){
 		return amount;
+	}
+	
+	public String cipherMobile(/*recebe uma/duas key*/){
+		String message;
+		message=iban+"-"+amount+"-"+"digest";//inserir o digest cifrado com a chave privada do sender assumindo '-' nao usados no digest
+		//cifrar message com chave publica do receiver
+		return message;
+	}
+	
+	public void setSignature(/*recebe private key para signature*/){
+		String elements=iban.concat(amount);
+		try {
+			
+			sig = Signature.getInstance("SHA256withRSA"); //tem de ser 256 porque e o suportado pelo java
+			//sig.initSign(/*recebe private key para signature*/);
+			sig.update(elements.getBytes());
+			
+		} catch (SignatureException e) {
+			System.out.println("Error while creating the signature");
+			e.printStackTrace();
+		}
+		catch (NoSuchAlgorithmException e) {
+			System.out.println("Error while creating the signature");
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
