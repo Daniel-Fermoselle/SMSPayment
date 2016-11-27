e60fe84be2c09932accdbb94e2acb4c7cd3e88be
TiaGoMSR <tiagomsr4@gmail.com>
2016-11-27 09:23:52 +0000

    Implemented Account for users' authentication

--- server/src/main/java/pt/sirs/server/Account.java
+++ server/src/main/java/pt/sirs/server/Account.java
@@ -6,26 +6,14 @@ public class Account{
 	
 	private String iban;
 	private int balance;
+	private String username;
+	private String password;
 
-	public Account(String iban, int balance){
-		this.setIban(iban);
-		this.setBalance(balance);
-	}
-
-	public String getIban() {
-		return iban;
-	}
-
-	public void setIban(String iban) {
+	public Account(String iban, int balance, String username, String password){
 		this.iban = iban;
-	}
-
-	public int getBalance() {
-		return balance;
-	}
-
-	public void setBalance(int balance) {
 		this.balance = balance;
+		this.username = username;
+		this.password = password;
 	}
 	
 	public void debit(int amount){
@@ -38,6 +26,38 @@ public class Account{
 	
 	public void credit(int amount){
 		this.balance = this.balance + amount;
+	}
+	
+	public String getIban() {
+		return iban;
+	}
+	
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
