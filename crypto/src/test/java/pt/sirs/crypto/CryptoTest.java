1e328734a2062b42b8a3d75ef6d5d262fc83d9d3
JoaoBraveCoding <joao.marcal12@hotmail.com>
2016-12-02 17:49:33 +0000

    Finished crypto test for presentation

--- crypto/src/test/java/pt/sirs/crypto/CryptoTest.java
+++ crypto/src/test/java/pt/sirs/crypto/CryptoTest.java
@@ -7,11 +7,16 @@ import javax.crypto.spec.SecretKeySpec;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-
-
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class CryptoTest extends AbstractCryptoTest{
   
+	private static final String PRIV_KEY_1_FILE_PATH = "keys/TestKey1PrivateKey";
+	private static final String PUB_KEY_1_FILE_PATH = "keys/TestKey1PublicKey";
+	private static final String PRIV_KEY_2_FILE_PATH = "keys/TestKey2PrivateKey";
+	private static final String PUB_KEY_2_FILE_PATH = "keys/TestKey2PublicKey";
+
 	private  static final SecretKeySpec SHARED_KEY1 = new SecretKeySpec(Crypto.decode("Y3gNMPdxtqc6F0YCJKu2gg=="),"AES");
 
 	private  static final SecretKeySpec SHARED_KEY2 = new SecretKeySpec(Crypto.decode("RLai0vYrXHkQFrDecgvSTw=="),"AES");
@@ -21,10 +26,15 @@ public class CryptoTest extends AbstractCryptoTest{
 	
 	private static final String PT_1_CIPHERED_K1 = "7LYcFbNpyHrQ8dXcCeCK+w==";
 	private static final String PT_1_CIPHERED_K1_CORRUPTED = "7LYcFbNpyHrQ8dXcCEcK+w==";
-
 	private static final String PT_2_CIPHERED_K1 = "DjW1EA3wd2TTlIeJeY5Hbg==";
+	
+	private static final String PT_1_SIG_PK1 = "MC0CFBGujb0pi5I3eM+LRyM/v2/KdftZAhUArfMTtDxlKXBU8ZZfa8cSgGCORuQ=";
+	private static final String PT_2_SIG_PK1 = "MC0CFQClnoa1ayzUBZWoUBJSuP+6/053dAIUdW4l+jdupqP7hxqLH6upESSZp5g=";
+	private static final String PT_1_SIG_PK2 = "MC4CFQC1lXymuMQU/kNuqStxPwX2BwQJLQIVAKUE5h+AsaHvW0UexNwJNUcnvRTp";
+	private static final String PT_1_SIG_PK1_CORRUPTED = "MC0CFBGujb0Pi5I3eM+LRyM/v2/KdftZAhUArfMTtDxlKXBU8ZZfa8cSgGCORuQ=";
 
 
+	
 	@Override
 	protected void populate() {
 
@@ -76,5 +86,27 @@ public class CryptoTest extends AbstractCryptoTest{
 	public void decipher_corrupted_cipher() throws Exception {
 		Crypto.decipherSMS(Crypto.decode(PT_1_CIPHERED_K1_CORRUPTED), SHARED_KEY1);
 	}
+	
+	@Test
+	public void verify_Sign_success() throws Exception {
+		assertTrue(Crypto.verifySign(PLAINTEXT_1, Crypto.decode(PT_1_SIG_PK1), Crypto.readPubKeyFromFile(PUB_KEY_1_FILE_PATH)));
+	}
+	
+	@Test
+	public void verify_Sign_wrong_msg() throws Exception {
+		assertFalse(Crypto.verifySign(PLAINTEXT_1, Crypto.decode(PT_2_SIG_PK1), Crypto.readPubKeyFromFile(PUB_KEY_1_FILE_PATH)));
+	}
+	
+	@Test
+	public void verify_Sign_wrong_key() throws Exception {
+		assertFalse(Crypto.verifySign(PLAINTEXT_1, Crypto.decode(PT_1_SIG_PK2), Crypto.readPubKeyFromFile(PUB_KEY_1_FILE_PATH)));
+	}
+	
+	@Test
+	public void verify_Sign_corrupted() throws Exception {
+		assertFalse(Crypto.verifySign(PLAINTEXT_1, Crypto.decode(PT_1_SIG_PK1_CORRUPTED), Crypto.readPubKeyFromFile(PUB_KEY_1_FILE_PATH)));
+	}
+	
+	
 	
 }
